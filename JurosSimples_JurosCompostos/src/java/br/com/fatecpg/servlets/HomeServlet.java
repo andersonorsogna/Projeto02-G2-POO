76c8547d4b803f51003ac69b17a089336dffc84e
andersonorsogna <andersen.orsogna@gmail.com>
2017-03-26 17:54:33 -0300

    Atualização Conteúdo
    Atualização do Conteúdo HomeServlets
    
    -Inserção dos integrantes
    -Inserção da imagem financeiro022
    Inserção da apresentação da aplicação

--- JurosSimples_JurosCompostos/src/java/br/com/fatecpg/servlets/HomeServlet.java
+++ JurosSimples_JurosCompostos/src/java/br/com/fatecpg/servlets/HomeServlet.java
@@ -42,14 +42,33 @@ public class HomeServlet extends HttpServlet {
             out.println("<body>");
             
             out.println("<Table border='1'>");
+            
             out.println("<tr>");
-            out.println("<td>");
+            out.println("<td colspan='2'>");
             out.println("<img src='Img/icon-fin-pb.jpg' width='20' height='20'/>");
             out.println("Juros Simples - Juros Composto");
             out.println("</td>");
+            out.println("</tr>");
+            
+            out.println("<tr>");
             out.println("<td>");
+            out.println("Integrantes: <br/><br/>-Anderson Orsogna Esteves<br/>-André<br/>-João<br/>-Raul<br/>");
+            out.println("</td>");
+            out.println("<td>");
+            out.println("<img src='Img/financeiro022.jpg'/>");
             out.println("</td>");
             out.println("</tr>");
+            
+            out.println("<tr>");
+            out.println("<td colspan='2'>");
+            out.println("Aplicação<br/><br/>"
+                    + "<u><b>Juros Simples:</u></b><br/>"
+                    + "O regime de juros será simples quando o percentual de juros incidir apenas sobre o valor principal. Sobre os juros gerados a cada período não incidirão novos juros. Valor Principal ou simplesmente principal é o valor inicial emprestado ou aplicado, antes de somarmos os juros.<br/>"
+                    + "<u><b>Juros Composto:</u></b><br/>"
+                    + "O regime de juros compostos é o mais comum no sistema financeiro e portanto, o mais útil para cálculos de problemas do dia-a-dia. Os juros gerados a cada período são incorporados ao principal para o cálculo dos juros do período seguinte.");
+            out.println("</td>");
+            out.println("</tr>");
+            
             out.println("</Table>");
             
             out.println("</body>");
