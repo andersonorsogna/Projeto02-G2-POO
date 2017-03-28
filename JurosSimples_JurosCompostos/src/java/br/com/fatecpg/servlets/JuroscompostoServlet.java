e145ebdec938352a8b82a44c8b0e46e55dfffc9c
João Augusto <>
2017-03-28 20:40:42 -0300

    Inicialização da estrutura juros compostos

--- JurosSimples_JurosCompostos/src/java/br/com/fatecpg/servlets/JuroscompostoServlet.java
+++ JurosSimples_JurosCompostos/src/java/br/com/fatecpg/servlets/JuroscompostoServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.DecimalFormat;
 
 /**
  *
@@ -37,20 +38,102 @@ public class JuroscompostoServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title> :: Juros Composto ::</title>");            
+            out.println("<title> ::Juros Compostos ::</title>");            
             out.println("</head>");
             out.println("<body>");
+           
+               float c = 0;
+                try{ c = Float.parseFloat(request.getParameter("c"));
+                }catch(Exception ex){}
+                
+               float t = 0;
+                try{ t = Float.parseFloat(request.getParameter("t"));
+                }catch(Exception ex){}
+               
+               DecimalFormat cj = new DecimalFormat("#.00"); 
+               DecimalFormat cm = new DecimalFormat("#.00");
+               
+               int n = 0;
+                try{ n = Integer.parseInt(request.getParameter("n"));
+                }catch(Exception ex){}
+                Float j = c*( +(t*n));
+                Float m = j/100 + c;
             
             out.println("<center>");
-            
+           
             out.println("<h1>");
             out.println("<img src='Img/icon-fin-pb.jpg' width='30' height='30'/>");
-            out.println("Juros Composto");
+            
+            out.println("Juros Simples");
             out.println("</h1>");
             
             out.println("</center>");
             
-            out.println("<a href='home.html'><img src='Img/btn-vt.jpg'/></a>");
+            out.println("<fieldset>");
+            out.println("<legend>Juros Simples</legend>");
+            out.println("<form>");
+            
+            out.println("<p>");
+            
+            out.println("<Table>");
+                                
+            out.println("<tr>");
+            out.println("<td align=right>");
+            out.println("<label>Capital (R$):</label>");
+            out.println("</td>");
+            out.println("<td>");
+            out.println("<input type='text' name='c' maxlength='20' placeholder='ex:25000'"+c+"'/>");
+            out.println("</td>");
+            out.println("</tr>");
+            
+            out.println("<tr>");
+            out.println("<td align=right>");
+            out.println("<label>Taxa de Juros (%):</label>");
+            out.println("</td>");
+            out.println("<td>");
+            out.println("<input type='text' name='t' maxlength='20' placeholder='ex:15'"+t+"'/>");
+            
+            out.println("</td>");
+            out.println("</tr>");
+            
+            out.println("<tr>");
+            out.println("<td align=right>");
+            out.println("<label>Número de Períodos (Meses):</label>");
+            out.println("</td>");
+            out.println("<td>");
+            out.println("<input type='text' name='n' maxlength='20' placeholder='ex:12'"+n+"'/>");
+            out.println("</td>");
+            out.println("</tr>");
+            
+            
+            
+            out.println("<tr>");
+            out.println("<td colspan='3' align=center>");
+            out.println("<p><input type='submit' value='Calcular'/>");
+            //t = t/100;
+            //c = c * t * n;
+            //j = c;
+            
+            out.println("</td>");
+            out.println("</tr>");
+            
+            out.println("<tr>");
+            
+            out.println("<td><b>Juros Total (R$): "+ cj.format(j/100) +"</td>");
+            
+            out.println("<td><b>Montante Total (R$): "+ cm.format(m) +"</td>");
+        
+            out.println("</tr>");
+            
+            out.println("</Table>");
+            out.println("</form>");
+            out.println("</fieldset>");
+                        
+            out.println("<br/><a href='home.html'><img src='Img/btn-vt.jpg'/></a>");
+            
+            //for(int i=1; i<=3; i++){
+            //out.println("<u>"+j+"</u>");
+            //}
             
             out.println("</body>");
             out.println("</html>");
